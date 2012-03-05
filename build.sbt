51e36bc812ead0034538875e11119546a2fc9d6c
Tommy C. Li <tommycli@ucla.edu>
2012-03-04 17:59:26 -0700

    Update plugins and build def

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.1.0-SNAPSHOT"
 scalaVersion := "2.9.1"
 
 seq(lwjglSettings: _*)
+
+seq(slickSettings: _*)
---
+++ src/main/scala/terrain/Heightmap.scala
@@ -0,0 +1,6 @@
+package edu.stanford.cs248.project.terrain
+
+trait TerrainMap {
+  def xSize: Int
+  def ySize: Int
+}
