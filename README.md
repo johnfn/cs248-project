f1cf000507540bc490590c5daa2a4d1021442164
Grant Mathews <johnfn@gmail.com>
2012-03-14 20:52:45 -0800

    Add assets, jpct, README, and first work on model loading.

---
+++ README.md
@@ -0,0 +1,7 @@
+# cs248 final project
+
+A game written in Scala.
+
+## Thanks to
+
+* `jPCT` - www.jpct.net
--- src/main/scala/Crystal.scala
+++ src/main/scala/Crystal.scala
@@ -12,26 +12,70 @@ import org.lwjgl._
 import input._
 import Keyboard._
 import scala.math._
+import com.threed.jpct._
+import com.threed.jpct.util._
+import math._
 
 class SquareModel(val x: Float, val y: Float, val z: Float) extends VBOModel {
 
 	val name = "cubemodel"
 
 	override def getVertices() = {
-	    val floorCorners = Array((-0.5f, -0.5f), (0.5f, -0.5f), (0.5f, 0.5f), (-0.5f, 0.5f))
+		val floorCorners = Array((-0.5f, -0.5f), (0.5f, -0.5f), (0.5f, 0.5f), (-0.5f, 0.5f))
 
-	    floorCorners.map { case(dx, dy) =>
+		floorCorners.map { case(dx, dy) =>
 			Vertex(dx, dy, z,
-				  0, 0, 1,
-				  dx + 0.5f, dy + 0.5f)
-			}
+				0, 0, 1,
+				dx + 0.5f, dy + 0.5f)
+		}
 	}
 
 	override def getIndices() = (0 until 4)
 }
 
-class Crystal(val x: Float, val y: Float, val z: Float) extends VBOModelEntity {
-	val model = new SquareModel(x, y, z) // old color: , List(0, 0, 250))
+class Crystal(val x: Float, val y: Float, val z: Float) extends Entity{
+	//val model = new SquareModel(x, y, z) // old color: , List(0, 0, 250))
+
+	override def doInitGL() = {
+		var plant:Object3D = null;
+
+		/*
+		Texture tex=new Texture("textures"+File.separatorChar+"plant.jpg");
+		TextureManager.getInstance().addTexture("plant", tex);
+
+		Texture tex2=new Texture("textures"+File.separatorChar+"plant2.jpg");
+		TextureManager.getInstance().addTexture("plant2", tex2);
+		*/
+
+		var objs:Array[Object3D] = Loader.load3DS("models/plant.3ds",2.5f);
+		if (objs.length==1) {
+			plant=objs(0);
+			plant.setTexture("plant");
+			plant.setTransparency(2);
+			plant.setCulling(Object3D.CULLING_DISABLED);
+			plant.rotateX(-3.14159f/2f);
+			plant.rotateMesh();
+			plant.setRotationMatrix(new Matrix());
+			//plant.setAdditionalColor(new Color(100,100,100));
+			plant.build();
+		} else {
+			println("Error loading crystal.")
+			System.exit(-1)
+		}
+	}
+
+	override def renderGL(shader: Shader) = {
+		import GL11._
+
+		glColor3f(1, 0, 0)
+
+		glBegin(GL_TRIANGLES)
+			glVertex3f(0, 0, 0)
+			glVertex3f(1, 0, 0)
+			glVertex3f(0, 1, 0)
+		glEnd()
+
+	}
 
 	override def traits() = List("render", "update", "crystal")
 }
