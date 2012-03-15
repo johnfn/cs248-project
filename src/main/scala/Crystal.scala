04316d0d5c6223e6b8786699c00b10729dd5cf22
Grant Mathews <johnfn@gmail.com>
2012-03-14 21:31:25 -0800

    Massive plant off the side of the screen.
    I think some verticies are being translated too many times.

--- src/main/scala/Crystal.scala
+++ src/main/scala/Crystal.scala
@@ -35,9 +35,10 @@ class SquareModel(val x: Float, val y: Float, val z: Float) extends VBOModel {
 
 class Crystal(val x: Float, val y: Float, val z: Float) extends Entity{
 	//val model = new SquareModel(x, y, z) // old color: , List(0, 0, 250))
+	var plant:Object3D = null;
+	var polymanager:PolygonManager = null;
 
 	override def doInitGL() = {
-		var plant:Object3D = null;
 
 		/*
 		Texture tex=new Texture("textures"+File.separatorChar+"plant.jpg");
@@ -49,15 +50,16 @@ class Crystal(val x: Float, val y: Float, val z: Float) extends Entity{
 
 		var objs:Array[Object3D] = Loader.load3DS("models/plant.3ds",2.5f);
 		if (objs.length==1) {
-			plant=objs(0);
-			plant.setTexture("plant");
-			plant.setTransparency(2);
-			plant.setCulling(Object3D.CULLING_DISABLED);
-			plant.rotateX(-3.14159f/2f);
-			plant.rotateMesh();
-			plant.setRotationMatrix(new Matrix());
-			//plant.setAdditionalColor(new Color(100,100,100));
-			plant.build();
+			plant=objs(0)
+			plant.setTexture("plant")
+			plant.setTransparency(2)
+			plant.setCulling(Object3D.CULLING_DISABLED)
+			plant.rotateX(-3.14159f/2f)
+			plant.rotateMesh()
+			plant.setRotationMatrix(new Matrix())
+			//plant.setAdditionalColor(new Color(100,100,100))
+			plant.build()
+			polymanager = plant.getPolygonManager()
 		} else {
 			println("Error loading crystal.")
 			System.exit(-1)
@@ -70,9 +72,11 @@ class Crystal(val x: Float, val y: Float, val z: Float) extends Entity{
 		glColor3f(1, 0, 0)
 
 		glBegin(GL_TRIANGLES)
-			glVertex3f(0, 0, 0)
-			glVertex3f(1, 0, 0)
-			glVertex3f(0, 1, 0)
+			for (poly <- 0 until polymanager.getMaxPolygonID(); vert <- 0 until 3) {
+				val sv:SimpleVector = polymanager.getTransformedVertex(poly, vert)
+				glVertex3f(sv.x, sv.y, sv.z)
+				println(poly + " " + vert)
+			}
 		glEnd()
 
 	}
